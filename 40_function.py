a54db0e273a2502825bd834b552e881416f3a3a7
Ambika <ambikagupta8899@gmail.com>
2026-08-13 17:43:56 +0530

    function 2

---
+++ 40_function.py
@@ -0,0 +1,8 @@
+def sum (a, b):
+    addi = a+b
+    return addi
+
+x = int(input("Enter fisrt number:"))
+y = int(input("Enter second number"))
+result = sum(x,y)
+print(f"The answer of addition is : {result}")
