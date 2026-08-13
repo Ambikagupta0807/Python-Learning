abec3e9ad17a278662f73deb0541550615221e94
Ambika <ambikagupta8899@gmail.com>
2026-08-13 18:50:40 +0530

    changed function 6

--- 45_function.py
+++ 45_function.py
@@ -4,3 +4,11 @@ def sum_all(*args):
 
 print(sum_all(1,3,6,2,4,8))
 
+def sum(*args):
+    print(*args)
+    print(args)
+    for i in args:
+        print(i*2)
+    return sum(args)
+
+print(sum(1,2,3))
