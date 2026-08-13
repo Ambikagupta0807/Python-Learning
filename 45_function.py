4648aefb272901fcf8e0c24d7345e0b8b724052c
Ambika <ambikagupta8899@gmail.com>
2026-08-13 18:52:28 +0530

    correct funnction 6

--- 45_function.py
+++ 45_function.py
@@ -4,11 +4,11 @@ def sum_all(*args):
 
 print(sum_all(1,3,6,2,4,8))
 
-def sum(*args):
+def addi(*args):
     print(*args)
     print(args)
     for i in args:
         print(i*2)
     return sum(args)
 
-print(sum(1,2,3))
+print(addi(1,2,3))
